6e9727e491364f820a5ff3873c28224fd66d8016
Adnan Khan <adnan7.zain@gmail.com>
2021-06-24 09:54:06 +0500

    class based implementation

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+  }
+}
+
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
