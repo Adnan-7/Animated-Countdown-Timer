27ca5ca4d1f0aab936621e4f855b6767da49fffa
Adnan Khan <adnan7.zain@gmail.com>
2021-06-24 16:51:18 +0500

    add animated circle using SVG and style.css

--- timer.js
+++ timer.js
@@ -1,27 +1,50 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callback) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    if (callback) {
+      this.onStart = callback.onStart;
+      this.onTick = callback.onTick;
+      this.onComplete = callback.onComplete;
+    }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 10);
   };
 
-  tick = () => {
-    if (this.durationInput.value <= 0) {
-      this.pause;
-    } else {
-      const timeRemaining = parseFloat(this.durationInput.value);
-      this.durationInput.value = timeRemaining - 1;
-    }
-  };
   pause = () => {
     clearInterval(this.interval);
   };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.01;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
 }
